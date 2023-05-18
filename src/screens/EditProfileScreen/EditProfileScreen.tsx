0b3df879624478c8d53b5d2b5985ca8d44917d4b
Akshay Sharma1 <akshay.sharma1@grazitti.local>
2023-05-18 12:28:24 +0530

    Done basic implementation of react hook form at editprofilescreen now need to fix typescript

--- src/screens/EditProfileScreen/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen/EditProfileScreen.tsx
@@ -1,37 +1,68 @@
 import { View, Text,StyleSheet,Image, TextInput } from 'react-native'
 import React from 'react'
+import { useForm , Controller, Control} from 'react-hook-form'
 import user from '../../assets/data/user.json'
 import colors from '../../theme/colors'
 import fonts from '../../theme/fonts'
+import { IUser } from '../../types/models'
 
 interface ICustomInput {
+    control:Control;
     label: string;
+    name:string;
     multiline?: boolean;
 }
 
-const CustomInput =({label,multiline= false}:ICustomInput) => (
-    <View style={styles.inputContainer} >
-        <Text style={styles.label}>{label}</Text>
-        <TextInput style={styles.input} placeholder='Hello' multiline={multiline}/>
-    </View>
-    
+const CustomInput =
+({
+    control,
+    label,
+    name,
+    multiline= false,
+}:ICustomInput) => (
+    // 3.Here we have to wrapp the textinput with the controller hook form so 
+    // that we can use function with textinput  
+    <Controller 
+        control={control} // type
+        name={name} //name of the field we are rendering rn
+        //Here we gonna put our custom input inside render function to render diff fields
+        render = {({field: {onChange,value,onBlur} }) => (
+            <View style={styles.inputContainer} >
+                <Text style={styles.label}>{label}</Text>
+                <TextInput
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                style={styles.input} 
+                placeholder='Hello' 
+                multiline={multiline}
+                />
+            </View>
+        )
+    }
+    />  
 )
 
 const EditProfileScreen = () => {
 
-    const onSubmit = () =>{
-        console.warn('submit')
+//1. control field :
+//will help use to bind one input to a value that which is gonna be managed by useForm
+    const {control,handleSubmit} = useForm();
+
+    const onSubmit = (data) =>{
+        console.log('data',data)
     }
   return (
     <View style={styles.page}>
         <Image source={{ uri :  user.image}} style={styles.avatar} />
         <Text style={styles.textButton} >Change Profile Photo</Text>
-        <CustomInput label={'Name'}/>
-        <CustomInput label={'Username'}/>
-        <CustomInput label={'website'}/>
-        <CustomInput label={'Bio'} multiline/>
-
-        <Text onPress={onSubmit} style={styles.textButton}>
+        <CustomInput name={'name'} label={'Name'} control={control}/>
+        <CustomInput name={'username'} label={'Username'} control={control}/>
+        <CustomInput name={'website'} label={'Website'} control={control} />
+        <CustomInput name={'bio'} label={'Bio'} multiline control={control}/>
+{/* 2. Here we are wrapping submit so that it will validate the fields and give us error 
+if any after that it will call the onSubmit function */}
+        <Text onPress={handleSubmit(onSubmit)} style={styles.textButton}>
         Submit
         </Text>
 
