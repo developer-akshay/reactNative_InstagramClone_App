6d32946321612e02fa5e65fe47e68f14ebd15eb8
Akshay Sharma1 <akshay.sharma1@grazitti.local>
2023-05-24 12:56:42 +0530

    Implemented stack navigation and played little bit about stack scrren,group and passing options for screens will implement navigation on click somewhere post this

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 // import { StyleSheet, View,SafeAreaView,ScrollView, FlatList } from "react-native";
 // import HomeScreen from "./src/screens/HomeScreen";
 // import CommentsScreen from "./src/screens/CommentsScreen/CommentsScreen";
-// import ProfileScreen from "./src/screens/ProfileScreen";
+
 // import EditProfileScreen from "./src/screens/EditProfileScreen";
 // import PostUploadScreen from "./src/screens/PostUploadScreen";
 import Navigation from "./src/navigation";
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,13 +1,52 @@
 import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 import React from 'react'
+import { Text, View,Image } from "react-native";
+import logo from '../assets/images/logo.png';
+
+// this will create stak navigator which will return a object with navigator and screen
+const Stack =createNativeStackNavigator(); // return object with {Navigator,Screen}
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-        <HomeScreen />
+        <Stack.Navigator 
+        initialRouteName="Feed"
+        screenOptions={{headerShown:true}}
+        >
+            {/* we use stack group to pass common propeties to the group other 
+            then the left screen it will also recive screenOptions*/}
+            {/* <Stack.Group screenOptions={{}}> */}
+                {/* stack.screen requires 2 properties  1 name 2 */}
+            <Stack.Screen 
+            name="Feed" 
+            component={HomeScreen}
+            //We made custom header for the screen
+            options={{headerTitle:HeaderTitle,headerTitleAlign:'center'}}
+            />
+
+            {/* we use title option to override the name  */}
+            <Stack.Screen 
+            name="userProfile" 
+            component={ProfileScreen} 
+            options={{title: "Profile"}} 
+            />
+            {/* </Stack.Group> */}
+            
+        </Stack.Navigator>
     </NavigationContainer>
   )
+}
+
+const HeaderTitle = () => {
+    return <Image 
+    source={logo} 
+    resizeMode='contain'
+    style={{width:120, height:50}} 
+    />;
+
 }
 
 export default Navigation;
